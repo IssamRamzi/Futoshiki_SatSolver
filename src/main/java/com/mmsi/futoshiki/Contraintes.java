0a6ac5cf221ee67ff9eddfbdd9dcfb22353bc9a2
Boubeker Melissa <Melissa.Boubeker@etu.univ-grenoble-alpes.fr>
2025-05-02 12:40:47 +0200

    j'ai ajouter la classe verifierH

--- src/main/java/com/mmsi/futoshiki/Contraintes.java
+++ src/main/java/com/mmsi/futoshiki/Contraintes.java
@@ -1,15 +1,26 @@
 package com.mmsi.futoshiki;
 
 public class Contraintes {
+    
+    public boolean verifierH(){
+        for ( int i =0 ; i < nombre.cases; i++;){
+            for (int j =0; j< nombre.cases; j++; ){
+              for(int k = j+1; k< nombre.cases; k++;){
+                if( case[i][j].value ==case[i][k].value) {
+                    return false ;
+                }
+              }
+            }
+        }  
+        return true;
+    }
+
     public boolean verifierV(){
         return false;
     }
 
-    public boolean verifierH(){
-        return false;
-    }
-
     public boolean verifierI(){
+        
         return false;
     }
 
